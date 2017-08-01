66fde1da3ac1d75a94bfccaf2617a469da97d412
aaronASC4 <aarongraves07@gmail.com>
2017-08-01 14:46:13 -0400

    test

---
+++ aaron-demopitch/sketch.js
@@ -0,0 +1,15 @@
+function RandomWord() {
+    var requestStr = "http://randomword.setgetgo.com/get.php";
+
+    $.ajax({
+        type: "GET",
+        url: requestStr,
+        dataType: "jsonp",
+        jsonpCallback: 'RandomWordComplete'
+    });
+}
+
+function RandomWordComplete(data) {
+    console.log(data);
+
+}
