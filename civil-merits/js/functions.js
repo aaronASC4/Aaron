545fb0d2424e2235a1102954779ea16f72a244c7
aaronASC4 <aarongraves07@gmail.com>
2017-08-15 16:38:36 -0400

    update

--- civil-merits/js/functions.js
+++ civil-merits/js/functions.js
@@ -82,3 +82,118 @@
 		
 
 })(jQuery);
+ var map;
+      var infowindow;
+
+      function initMap() {
+        var pyrmont = {lat: 40.7484410, lng: -73.9856640};
+
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: pyrmont,
+          zoom: 15
+        });
+
+        infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
+          location: pyrmont,
+          radius: 1000,
+          type: ['police']
+        }, callback);
+      }
+
+      function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+          }
+        }
+      }
+
+      function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
+        });
+      }
+'use strict';
+
+var vgaButton = document.querySelector('button#vga');
+var qvgaButton = document.querySelector('button#qvga');
+var hdButton = document.querySelector('button#hd');
+var dimensions = document.querySelector('p#dimensions');
+var video = document.querySelector('video');
+
+navigator.getUserMedia = navigator.getUserMedia ||
+  navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+function successCallback(stream) {
+  window.stream = stream; // stream available to console
+  video.src = window.URL.createObjectURL(stream);
+}
+
+function errorCallback(error) {
+  console.log('navigator.getUserMedia error: ', error);
+}
+
+function displayVideoDimensions() {
+  dimensions.textContent = 'Actual video dimensions: ' + video.videoWidth +
+    'x' + video.videoHeight + 'px.';
+}
+
+video.addEventListener('play', function() {
+  setTimeout(function() {
+    displayVideoDimensions();
+  }, 500);
+});
+
+var qvgaConstraints = {
+  video: {
+    mandatory: {
+      maxWidth: 320,
+      maxHeight: 180
+    }
+  }
+};
+
+var vgaConstraints = {
+  video: {
+    mandatory: {
+      maxWidth: 640,
+      maxHeight: 360
+    }
+  }
+};
+
+var hdConstraints = {
+  video: {
+    mandatory: {
+      minWidth: 1280,
+      minHeight: 720
+    }
+  }
+};
+
+qvgaButton.onclick = function() {
+  getMedia(qvgaConstraints);
+};
+vgaButton.onclick = function() {
+  getMedia(vgaConstraints);
+};
+hdButton.onclick = function() {
+  getMedia(hdConstraints);
+};
+
+function getMedia(constraints) {
+  if (window.stream) {
+    video.src = null;
+    window.stream.getVideoTracks()[0].stop();
+  }
+  navigator.getUserMedia(constraints, successCallback, errorCallback);
+}
